80570814364bc07cf8fa55c8220170b0184a16d8
Imesh Gunathilaka <imesh562@gmail.com>
2026-05-11 01:14:40 +0530

    Changed configs to work with the latest flutter version.

--- lib/features/tasks/data/models/get_tasks_response.g.dart
+++ lib/features/tasks/data/models/get_tasks_response.g.dart
@@ -8,27 +8,26 @@ part of 'get_tasks_response.dart';
 
 GetTasksResponse _$GetTasksResponseFromJson(Map<String, dynamic> json) =>
     GetTasksResponse(
-      tasks: (json['tasks'] as List<dynamic>?)
+      tasks:
+          (json['tasks'] as List<dynamic>?)
               ?.map((e) => TaskItem.fromJson(e as Map<String, dynamic>))
               .toList() ??
           const [],
     );
 
 Map<String, dynamic> _$GetTasksResponseToJson(GetTasksResponse instance) =>
-    <String, dynamic>{
-      'tasks': instance.tasks,
-    };
+    <String, dynamic>{'tasks': instance.tasks};
 
 TaskItem _$TaskItemFromJson(Map<String, dynamic> json) => TaskItem(
-      userId: (json['userId'] as num?)?.toInt(),
-      id: (json['id'] as num?)?.toInt(),
-      title: json['title'] as String?,
-      completed: json['completed'] as bool?,
-    );
+  userId: (json['userId'] as num?)?.toInt(),
+  id: (json['id'] as num?)?.toInt(),
+  title: json['title'] as String?,
+  completed: json['completed'] as bool?,
+);
 
 Map<String, dynamic> _$TaskItemToJson(TaskItem instance) => <String, dynamic>{
-      'userId': instance.userId,
-      'id': instance.id,
-      'title': instance.title,
-      'completed': instance.completed,
-    };
+  'userId': instance.userId,
+  'id': instance.id,
+  'title': instance.title,
+  'completed': instance.completed,
+};
